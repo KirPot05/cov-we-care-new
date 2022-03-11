447af173cd3033e0b6324aa8fef41f8f61301ea1
Kiran <kiran0503@gmail.com>
2022-03-11 17:08:04 +0530

    Deploying

---
+++ public/scripts/index.js
@@ -0,0 +1,54 @@
+const menu = document.getElementById('menu');
+const navbar = document.getElementById('navbar');
+const header = document.getElementById('header');
+const scrollIcon = document.getElementById('scrollIcon');
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header .navbar a');
+
+
+function stickyNav(){
+    if(window.scrollY > 0){
+        header.classList.add('sticky');
+        scrollIcon.style.display = "block"
+    } else{
+        header.classList.remove('sticky');
+        scrollIcon.style.display = "none";
+    }
+}
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle')
+});
+
+
+
+window.addEventListener("scroll", () => {
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle')
+
+    stickyNav();
+
+
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let height = section.offsetHeight;
+        let offset = section.offsetTop - 150;
+        let id = section.getAttribute('id');
+
+        console.log(section)
+
+        if(top >= offset  &&  top < offset + height){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+ id +']').classList.add('active');
+            })
+        }
+    })
+
+
+    
+
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const indexRouter = require('./routes/index');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
+
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+
+// Routes
+app.use('/', indexRouter)
+
+
+// Listening At Port
+app.listen(PORT, () => {
+    console.log("Listening at http://localhost:3000");
+});
