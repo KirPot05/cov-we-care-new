75608aacf59399b31b4ed397e834252ccd0b1e5b
Kiran <kiran0503@gmail.com>
2022-05-08 11:33:25 +0530

    configured firebase

--- models/firebase.js
+++ models/firebase.js
@@ -9,12 +9,13 @@ if(process.env.NODE_ENV !== 'production'){
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBBodOcjv0Pg3y45kjtVigZJnA87nIU0k0",
-    authDomain: "cov-we-care-7c2fe.firebaseapp.com",
-    projectId: "cov-we-care-7c2fe",
-    storageBucket: "cov-we-care-7c2fe.appspot.com",
-    messagingSenderId: "274522260662",
-    appId: "1:274522260662:web:18678f07dc2eaed3045217"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
